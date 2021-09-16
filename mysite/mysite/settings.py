39f35f7d44c616bd6879f00ad63413740e9a3ba4
Азиз Давлетов <davletovaziz511@gmail.com>
2021-09-16 15:25:14 +0600

    update debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,6 @@ DATABASES = {
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
